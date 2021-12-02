6ff4c9e874ccb1af7092243006ffd838a515ef4d
Alexander Sumiński <alex4723985@gmail.com>
2021-12-02 17:32:20 +0100

    (day 2) first star

--- Day02/src/Lib.hs
+++ Day02/src/Lib.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Lib
     ( someFunc
     ) where
 
+import           Data.List
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+        input <- lines <$> readFile "src/input.txt"
+        print $ countPosition input
+
+countPosition xs = (down - up) * forward
+  where
+    down = head result
+    forward = head $ tail result
+    up = head $ tail $ tail result
+    result = yellow xs
+
+yellow = map sum . map red . groupBy green . sort . map words
+
+red = map $ (\x -> read x::Int) . head . tail
+
+green a b = head a == head b
