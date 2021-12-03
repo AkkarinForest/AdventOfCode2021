8d3b167fbcd13c0a7f3c65840b91701eda43eb02
Alexander Sumiński <alex4723985@gmail.com>
2021-12-03 09:45:11 +0100

    (day 3) first star

--- Day03/src/Lib.hs
+++ Day03/src/Lib.hs
@@ -2,5 +2,23 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List
+import Data.Char (digitToInt)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+        input <- lines <$> readFile "src/input.txt"
+        print $ calculatePower input
+
+calculatePower xs = toDec epsilon * toDec gamma
+  where
+    gamma = map mostCommon $ transpose xs
+    epsilon = map flipBit gamma
+
+flipBit '0' = '1'
+flipBit '1' = '0'
+
+mostCommon :: Ord a => [a] -> a
+mostCommon = snd . maximum . map (\xs -> (length xs, head xs)) . group . sort
+
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
